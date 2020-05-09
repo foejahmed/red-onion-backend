357a8ad3530cbba189913951e5f61646e0983cae
foejahmed <foej.du@gmail.com>
2020-05-09 13:12:36 +0600

    fix nodemon error

--- index.js
+++ index.js
@@ -1,131 +1,112 @@
-const express=require('express');
-const bodyParser = require('body-parser')
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
-const cors = require('cors')
-const app=express();
+require('dotenv').config();
 
-app.use(cors())
-app.use(bodyParser.json())
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
 
 const uri = process.env.DB_PATH;
-let client = new MongoClient(uri, { useNewUrlParser: true }, { useUnifiedTopology: true });
+let client = new MongoClient(uri, { useNewUrlParser: true });
 
 
-//database connection
-// const uri = process.env.DB_PATH;
-// let client = new MongoClient(uri, { useNewUrlParser: true });
-// client.connect(err => {
-//     const collection = client.db("restaurant").collection("foodItems");   
-// });
-
-
-app.get("/fooditems", (req,res)=>{
-    client = new MongoClient(uri, { useNewUrlParser: true },{ useUnifiedTopology: true });
-
-    client.connect(err=>{
-        const collection = client.db("restaurant").collection("foodItems");
-        collection.find().toArray((err, documents)=>{
-            if(err){
-                console.log(err)
-                res.status(500).send({message: err})
-            }
-            else{
-                // console.log("sucessfully inserted", result)
-                res.send(documents)
-            }
-        })
-        // client.close();
-    })
-})
-
-app.post('/addfood', (req,res)=>{
-    const product= req.body
-    let uri = process.env.DB_PATH;
-    client = new MongoClient(uri, { useNewUrlParser: true }, { useUnifiedTopology: true });
+// Get
+app.get('/', (req, res) => {
+    res.send('<a href="/products">Products</a>');
+});
+app.get('/fooditems', (req, res) => {
+    client = new MongoClient(uri, { useNewUrlParser: true });
     client.connect(err => {
-        const collection = client.db("restaurant").collection("foodItems");
-        collection.insert(product, (err, result)=>{
-            if(err){
-                console.log(err)
-                res.status(500).send({message: err})
+        const collection = client.db("restaurant").collection("fooditems");
+        collection.find().toArray((err, documents) => {
+            if (err) {
+                console.log(err);
+                res.status(500).send({ message: err });
+            } else {
+                res.send(documents);
             }
-            else{
-                // console.log("sucessfully inserted", result)
-                res.send(result.ops[0])
-            }
-        })     
+        });
+        client.close();
     });
-})
+});
 
-app.get('/fooditems/:key', (req,res)=>{
-    const key= req.params.key
-    client = new MongoClient(uri, { useNewUrlParser: true },{ useUnifiedTopology: true });
-
-    client.connect(err=>{
-        const collection = client.db("restaurant").collection("foodItems");
-        collection.find({key: key}).toArray((err, documents)=>{
-            if(err){
-                console.log(err)
-                res.status(500).send({message: err})
-            }
-            else{
-                // console.log("sucessfully inserted", result)
-                res.send(documents[0])
-            }
-        })
-        // client.close();
-    })
-})
-
-app.post("/getFoodsByKey", (req,res)=>{
-    const key= req.params.key;
-    const productKeys= req.body;
-    console.log(productKeys)
+app.get('/fooditems/:key', (req, res) => {
+    const key = req.params.key;
 
     client = new MongoClient(uri, { useNewUrlParser: true });
-
-    client.connect(err=>{
-        const collection = client.db("restaurant").collection("foodItems");
-        collection.find({key: {$in: productKeys}}).toArray((err, documents)=>{
-            if(err){
-                console.log(err)
-                res.status(500).send({message: err})
+    client.connect(err => {
+        const collection = client.db("restaurant").collection("fooditems");
+        collection.find({ key }).toArray((err, documents) => {
+            if (err) {
+                console.log(err);
+                res.status(500).send({ message: err });
+            } else {
+                res.send(documents[0]);
             }
-            else{
-                // console.log("sucessfully inserted", result)
-                res.send(documents)
-            }
-        })
-        // client.close();
-    })
-})
+        });
+        client.close();
+    });
+});
 
-app.post("/placeOrder", (req,res)=>{
-    // console.log("data received ", req.body);
-    //save to database
-    const orderDetails= req.body;
-    orderDetails.orderTime= new Date()
-
-    console.log(orderDetails);
-    // console.log(product);
+// Post
+app.post('/getfooditemsByKey', (req, res) => {
+    const key = req.params.key;
+    const productKeys = req.body;
 
     client = new MongoClient(uri, { useNewUrlParser: true });
+    client.connect(err => {
+        const collection = client.db("restaurant").collection("fooditems");
+        collection.find({ key: { $in: productKeys } }).toArray((err, documents) => {
+            if (err) {
+                console.log(err);
+                res.status(500).send({ message: err });
+            } else {
+                res.send(documents);
+            }
+        });
+        client.close();
+    });
+});
 
-    client.connect(err=>{
+app.post('/addfooditems', (req, res) => {
+    const product = req.body;
+
+    client = new MongoClient(uri, { useNewUrlParser: true });
+    client.connect(err => {
+        const collection = client.db("restaurant").collection("fooditems");
+        collection.insert(product, (err, result) => {
+            if (err) {
+                console.log(err);
+                res.status(500).send({ message: err });
+            } else {
+                res.send(result.ops[0]);
+            }
+        });
+        client.close();
+    });
+});
+
+app.post('/placeOrder', (req, res) => {
+    const orderDetails = req.body;
+    orderDetails.orderTime = new Date();
+
+    client = new MongoClient(uri, { useNewUrlParser: true });
+    client.connect(err => {
         const collection = client.db("restaurant").collection("orders");
-        collection.insertOne(orderDetails, (err, result)=>{
-            if(err){
-                console.log(err)
-                res.status(500).send({message: err})
+        collection.insertOne(orderDetails, (err, result) => {
+            if (err) {
+                console.log(err);
+                res.status(500).send({ message: err });
+            } else {
+                res.send(result.ops[0]);
             }
-            else{
-                // console.log("sucessfully inserted", result)
-                res.send(result.ops[0])
-            }
-
-        })
-    })
-})
+        });
+        client.close();
+    });
+});
 
 const port = process.env.PORT || 4200;
-app.listen(port, () => console.log('listening to port 4200'));
+app.listen(port, () => console.log(`Listening to port`));
+
